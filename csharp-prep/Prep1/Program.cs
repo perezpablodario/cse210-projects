78b9f1690a34509eec385e99f39df0c666c8de72
pablodarioperez <perezpablodario@gmail.com>
2023-04-21 22:16:29 -0300

    I finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // variables to store the first and last name of the user
+        string first_name = "";
+        string last_name = "";
+
+        // Ask for the first name of the user
+        Console.Write("What is your first name? ");
+        first_name = Console.ReadLine();
+
+        // Ask for the last name of the user
+        Console.Write("What is your last name? ");
+        last_name = Console.ReadLine();
+
+        // Show the name 
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
