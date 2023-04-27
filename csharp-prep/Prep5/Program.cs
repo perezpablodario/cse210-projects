c9b293db915dafce2f28c9583b8610680ec2aa5c
pablodarioperez <perezpablodario@gmail.com>
2023-04-27 00:46:37 -0300

    I finished core requirements in prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squarenumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squarenumber);
+
+        
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+         static string PromptUserName()
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Enter your faborite number: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int squareNumber)
+        {
+            int square = squareNumber * squareNumber;
+            return square; 
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
     }
 }
