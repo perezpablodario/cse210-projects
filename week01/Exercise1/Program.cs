7c4e253a4f5c54600a3035bb2f8d7c6b662ef1a0
perezpablodario <perezpablodario@gmail.com>
2026-05-08 19:01:04 -0300

    Exercise one completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string Firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string Lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Lastname}, {Firstname} {Lastname}.");
     }
 }
