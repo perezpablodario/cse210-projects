683695a65749e0b6d8f598d9cd30a59513fbcb98
perezpablodario <perezpablodario@gmail.com>
2026-05-08 21:48:56 -0300

    Exercise 3 completed plus stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        string guess = "no";
+        string play = "yes";
+        int track = 0;
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        while (play == "yes")
+        {
+            guess = "no";
+            track = 0;
+            while (guess == "no")
+            {
+                track++;
+
+                Console.Write("What is your guess? ");
+                int guessNumber = int.Parse(Console.ReadLine());
+
+                if (number == guessNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You made {track} guesses!!!");
+                    guess = "yes";
+                }
+                else if (number > guessNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.Write("Do you want to play again? (yes / no) ");
+            play = Console.ReadLine();
+        }
+
     }
 }
