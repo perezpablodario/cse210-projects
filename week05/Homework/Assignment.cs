27902f936e8744ef10d673370bb7c366ace56957
perezpablodario <perezpablodario@gmail.com>
2026-06-01 11:58:58 -0300

    W05 Learning activity inheritance finished

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,17 @@
+public class Assignment
+{
+    protected string _studentName;
+    private string _topic;
+
+    public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"Student's name: {_studentName}, Topic: {_topic} ";
+    }
+
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine("***********************************************************");
+
+        MathAssignment mathAssignment2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment2.GetSummary());
+        Console.WriteLine(mathAssignment2.GetHomeworkList());
+
+        Console.WriteLine("***********************************************************");
+
+        WritingAssignment writingAssignment3 = new WritingAssignment("Mary Waters", "European History", "The causes of World War II");
+        Console.WriteLine(writingAssignment3.GetSummary());
+        Console.WriteLine(writingAssignment3.GetWritingInformation());
+
     }
 }
